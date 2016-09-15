1bd9b70735ebb86680bfcb71c010b58539473a58
Sean Hsu <sean_hsu@htc.com>
2016-09-15 14:27:58 +0000

    udpate add-client

--- add-client.sh
+++ add-client.sh
@@ -14,9 +14,11 @@ echo $2 | grep -iE "^$" || export OVPN_DATA=$2
 
 echo
 echo "adding client $1"
-docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full CLIENTNAME nopass
+docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full $1 nopass
 echo
 echo "exporting client info to $1.ovpn"
+docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient $1 > $1.ovpn
+
 echo
 echo "open w/ openvpn client, remeber enable 1194/udp in your server"
 echo
